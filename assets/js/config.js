595112a1fdf84d2e970abba6e3032eb56e5d23e9
Thomas <jarriert@gmail.com>
2020-04-14 13:57:47 +0200

    Modification du gitignore et ajout de la config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://127.0.0.1:8000/api/';
+export const API_URL = process.env.API_URL;
 export const CUSTOMERS_URL = API_URL + 'customers';
 export const INVOICES_URL = API_URL + 'invoices';
 export const USERS_URL = API_URL + 'users';
